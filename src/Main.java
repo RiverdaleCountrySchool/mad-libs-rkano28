7aa87a02897e14c8c4f5f1fd5bb1520f71762817
rkano28 <rkanovich28@riverdale.edu>
2025-01-28 11:32:33 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("This morning, I needed to go buy some " + args[0] + ".")
+        System.out.println("After that, I started " + args[1] + " to school.")
+        System.out.print(" The view was " + args[2] + ".")
+        System.out.print("On my way, I stopped by" + args[3] + ".")
+        System.out.println(" After eating a" + args[4] + ", I realized I was late for school.")
     }
 }
